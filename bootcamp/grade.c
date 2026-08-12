2b67f9d06a6922204841bb7534d047a9f7067f81
Samiul Anowar <262-16-064@diu.edu.bd>
2026-08-13 01:07:06 +0600

    Create grade.c

---
+++ bootcamp/grade.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int grade;
+    scanf("%d",&grade);
+
+    if(grade>=80 && grade<=100) printf("A");
+    else if(grade>=70 && grade<=79) printf("B");
+    else if(grade>=60 && grade<=69) printf("C");
+    else if(grade>=50 && grade<=59) printf("D");
+    else if(grade>=0 && grade<=49) printf("F");
+}
