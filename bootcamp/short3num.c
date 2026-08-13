3c6502f18492c86dae0908b5b267dcb314917c93
Samiul Anowar <262-16-064@diu.edu.bd>
2026-08-13 21:54:22 +0600

    short of three numbers

---
+++ bootcamp/short3num.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+
+    int temp;
+
+    if (a>b){
+        temp = a;
+        a = b;
+        b = temp;
+    }
+
+    if (a>c){
+        temp = a;
+        a = c;
+        c = temp;
+    }
+
+    if (b>c){
+        temp = b;
+        b = c;
+        c = temp;
+    }
+
+    printf("%d %d %d",a,b,c);
+
+}
