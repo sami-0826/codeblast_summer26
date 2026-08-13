f7803b510349f327a0d98a3f52bb83a2b85271ae
Samiul Anowar <262-16-064@diu.edu.bd>
2026-08-13 20:36:25 +0600

    determine if a number is prime

---
+++ bootcamp/check_prime.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    if (n < 2) {
+        printf("NO");
+        return 0;
+    }
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            printf("NO");
+            return 0;
+        }
+    }
+
+    printf("YES");
+
+    return 0;
+}
